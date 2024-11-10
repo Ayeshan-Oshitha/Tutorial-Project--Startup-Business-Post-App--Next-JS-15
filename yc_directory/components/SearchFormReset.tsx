b05692d2558ac5d210e79e4070baf811ef884397
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-11-10 12:23:31 +0530

    5. Search Form

---
+++ yc_directory/components/SearchFormReset.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { X } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const SearchFormReset = (props: Props) => {
+  const reset = () => {
+    const form = document.querySelector(".Search-form") as HTMLFormElement;
+
+    if (form) {
+      form.reset();
+    }
+  };
+  return (
+    <button type="reset" onClick={reset}>
+      <Link href="/" className="search-btn text-white">
+        <X className="size-5" />
+      </Link>
+    </button>
+  );
+};
+
+export default SearchFormReset;
