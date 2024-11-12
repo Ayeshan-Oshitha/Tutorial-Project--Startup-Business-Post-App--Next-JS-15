ac106d5d7896ece9c600838389c5c6b14b5fcea5
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-11-12 10:18:12 +0530

    18. User Profile

--- yc_directory/components/Navbar.tsx
+++ yc_directory/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { auth, signOut, signIn } from "@/auth";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AvatarFallback } from "./ui/avatar";
 
 type Props = {};
 
@@ -18,7 +21,8 @@ const Navbar = async (props: Props) => {
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
               <form
                 action={async () => {
@@ -26,10 +30,16 @@ const Navbar = async (props: Props) => {
                   await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">Logout</button>
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout </span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
               </form>
               <Link href={`/user/${session?.id}`}>
-                <span> {session?.user?.name}</span>
+                <Avatar>
+                  <AvatarImage className="size-10" src={session?.user?.image || ""} />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
--- yc_directory/components/StartupCard.tsx
+++ yc_directory/components/StartupCard.tsx
@@ -1,10 +1,12 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
+import { auth } from "@/auth";
 
 interface Props {
   post: StartupTypeCard;
@@ -34,7 +36,7 @@ const StartupCard: React.FC<Props> = ({ post }: Props) => {
           </Link>
         </div>
         <Link href={`/user/${author?._id}`}>
-          <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
+          <Image src={author?.image!} alt={author?.name!} width={48} height={48} className="rounded-full" />
         </Link>
       </div>
 
@@ -55,5 +57,15 @@ const StartupCard: React.FC<Props> = ({ post }: Props) => {
     </li>
   );
 };
+
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
 
 export default StartupCard;
